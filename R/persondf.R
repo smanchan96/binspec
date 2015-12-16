122dea1c19e8c6deabfaab2e62044aa89ed6a64f
smanchan96 <smanchan@purdue.edu>
2015-12-16 17:04:54 -0500

    Completed the reduceAndConcat method

--- R/persondf.R
+++ R/persondf.R
@@ -38,10 +38,32 @@ persondf.plot <- function(persondf){
 #' reduceAndConcat()
 reduceAndConcat <- function(...) {
   mmdb <- minMaxByDay(...)
-  rowlen = sum(sapply(1:nrow(mmdb), function(x) mmdb$max[x]-mmdb$min[x]+1))
-  mymatrix <- matrix(nrow=length(list(...)), ncol=rowlen)
+  rowlen <- sum(sapply(1:nrow(mmdb), function(x) mmdb$max[x]-mmdb$min[x]+1))
+  mymatrix <<- matrix(nrow=length(list(...)), ncol=rowlen)
   mydf <- data.frame("mz"=as.vector(unlist(sapply(1:nrow(mmdb), function(x) mmdb[x,1]:mmdb[x,2]))), "freq"=rep(0, rowlen), "ids"=rep("anon", rowlen), days=as.vector(unlist(sapply(1:nrow(mmdb), function(x) rep(x, mmdb[x,2]-mmdb[x,1]+1)))))
-  mydf
+  for (i in 1:nrow(mymatrix)) {
+    mymatrix[i,] <<- dfmerger(mydf, list(...)[[i]])
+  }
+  mymatrix
+}
+
+#' Reduce to one variable helper
+#'
+#' Takes in a data frame whose freq column hasn't been modified and fills it with matching rows from srcdf
+#' @param filldf 
+#' @param srcdf
+#' @export dfmerger
+#' @examples
+#' dfmerger()
+dfmerger <- function(filldf, srcdf) {
+  frq <- rep(0, nrow(filldf))
+  sapply(1:nrow(filldf), function(x) {
+    tmp <- srcdf$freq[srcdf$mz==filldf$mz[x] & srcdf$day==filldf$day[x]]
+    if (length(tmp)==1){
+      frq[x]<<-tmp
+    }
+})
+  frq
 }
 
 #' Returns the min and max of each day
