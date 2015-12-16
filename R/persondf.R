eb516fa2eb6968a1d523d85165d464275ceb6383
smanchan96 <smanchan@purdue.edu>
2015-12-16 16:45:32 -0500

    Made some progress on reduction method.  Also implemented min and max method.  Graphing works.

--- R/persondf.R
+++ R/persondf.R
@@ -28,3 +28,32 @@ persondf.plot <- function(persondf){
   require(lattice)
   xyplot(persondf$freq~persondf$mz|sapply(persondf$days, function(x)paste("Day ", x)), xlab="m/z", ylab="Frequency", main=persondf$id[1])
 }
+
+#' Reduce to one variable
+#'
+#' Reduces each persons m/z, frequencies, and days to just frequencies
+#' @param ... Each parameter should be a person df
+#' @export reduceAndConcat
+#' @examples
+#' reduceAndConcat()
+reduceAndConcat <- function(...) {
+  mmdb <- minMaxByDay(...)
+  rowlen = sum(sapply(1:nrow(mmdb), function(x) mmdb$max[x]-mmdb$min[x]+1))
+  mymatrix <- matrix(nrow=length(list(...)), ncol=rowlen)
+  mydf <- data.frame("mz"=as.vector(unlist(sapply(1:nrow(mmdb), function(x) mmdb[x,1]:mmdb[x,2]))), "freq"=rep(0, rowlen), "ids"=rep("anon", rowlen), days=as.vector(unlist(sapply(1:nrow(mmdb), function(x) rep(x, mmdb[x,2]-mmdb[x,1]+1)))))
+  mydf
+}
+
+#' Returns the min and max of each day
+#'
+#' Reduces each persons m/z, frequencies, and days to just frequencies
+#' @param ... Each arameter should be a person df
+#' @export minMaxByDay
+#' @examples
+#' minMaxByDay()
+minMaxByDay <- function(...) {
+  myDays <- unique(unlist(lapply(list(...), function(x) unique(x$days))))
+  mins <- sapply(myDays, function(x)min(unlist(lapply(list(...), function(y)min(y$freq[y$days==x])))))
+  maxs <- sapply(myDays, function(x)max(unlist(lapply(list(...), function(y)max(y$freq[y$days==x])))))
+  data.frame("min"=mins, "max"=maxs)
+}
