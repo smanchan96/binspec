727e9aa96e987ff111ec97258ab45262a98072eb
Sameer Manchanda <smanchan@llc.stat.purdue.edu>
2016-05-16 09:51:33 -0400

    Rounding and peak identification

--- R/preprocess.R
+++ R/preprocess.R
@@ -1,16 +1,39 @@
+#' Round data frame
+#'
+#' Find the intensity peaks
+#' @param df Data frame
+#' @export round_df
+#' @examples
+#' integer_round_df()
+
+round_df <- function(df) {
+    #Round each column to the nearest integer
+    as.data.frame(sapply(as.matrix(df), round))
+}
+
 #' Find binary peaks
 #'
 #' Find the intensity peaks
 #' @param df Data frame of m/z and intensities
 #' @param neighbors Number of neighboring m/z values to compare on right and left
-#' @param smooth Smoothing parameter
+#' @param error m/z measuring error parameter
 #' @export binary_peaks
 #' @examples
 #' binary_peaks()
 
-binary_peaks <- function(df, neighbors, smooth) {
-  sapply(1:nrow(df), function(x) {
-    window <- intersect((x - neighbors):(x + neighbors), 1:nrow(df))
-    all(df[[2]][x] >= df[[2]][window])
-  })
+binary_peaks <- function(df, neighbors, error=0) {
+    #For each m/z value, see if it is max of its neighbors
+    #peaks are true/false values
+    peaks <- sapply(1:nrow(df), function(x) {
+                    window <- intersect((x - neighbors):(x + neighbors), 1:nrow(df))
+                    all(df[[2]][x] >= df[[2]][window])
+})
+    #All the m/z values within the error of a peak should also be labled as peaks
+    peakmzs <- df[[1]][peaks]
+    #newpeaks are all o f
+    newpeaks <- unlist(sapply(peakmzs), function(x) {
+                      floor(x - error * x):ceiling(x + error * x) 
+
+})
 }
+
