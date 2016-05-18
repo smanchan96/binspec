914e73c2022704adb4a043e43cc69c453f30f905
Sameer Manchanda <smanchan@llc.stat.purdue.edu>
2016-05-18 12:45:36 -0400

    Document combine_peaks

--- R/preprocess.R
+++ R/preprocess.R
@@ -1,6 +1,6 @@
 #' Round data frame
 #'
-#' Find the intensity peaks
+#' Round all m/z and intensity values to integers.
 #' @param df Data frame
 #' @export round_df
 
@@ -11,13 +11,11 @@ round_df <- function(df) {
 
 #' Find binary peaks
 #'
-#' Find binary peaks
+#' Find peaks in window of size 2*neighbors + 1 and label m/z integers within the error as peaks.  Returns vector of peak m/z integers.
 #' @param df Data frame of m/z and intensities
 #' @param neighbors Number of neighboring m/z values to compare on right and left
-#' @param error m/z measuring error parameter
+#' @param error m/z Decimal error value
 #' @export binary_peaks
-#' @examples
-#' binary_peaks()
 
 binary_peaks <- function(df, neighbors, error=0) {
     #For each m/z value, see if it is max of its neighbors
@@ -36,5 +34,15 @@ binary_peaks <- function(df, neighbors, error=0) {
 })))
     #remove items that are not in original m/z range (assumes df[[1]] sorted)
     toReturn[toReturn >= df[[1]][1] & toReturn  <= df[[1]][nrow(df)]]
+}
+
+#' Combine peak vectors
+#'
+#' Create a binary matrix, each column represents and m/z value, and each row represents a mass spectra.  The value indicates whether or not the m/z of this spectra is a peak.
+#' @param list_mz_peaks List of m/z peak vectors
+#' @export combine_peaks
+
+combine_peaks <- function(list_mz_peaks)  {
+    NULL
 }
 
